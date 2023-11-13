6134a2aa59652787b0f25f321c4bd12c6ffc5cbb
Jonas Andreasson <jo7334an-s@student.lu.se>
2023-11-13 16:51:21 +0100

    Implemented more of lab2

--- lab2/intopt.c
+++ lab2/intopt.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
+#include <math.h>
+#define EPSILON __FLT_EPSILON__
 typedef struct simplex_t  simplex_t;
 struct simplex_t{
     int m;
@@ -14,6 +15,7 @@ struct simplex_t{
     double y;
     
 };
+int glob;
 
 double** make_matrix(int m, int n){
     double** a;
@@ -32,22 +34,28 @@ double* make_vector(int size){
 int select_nonbasic(simplex_t* s){
     int i;
     for(i=0; i < s->n; i++){
-        if(s->c[i] > 0.000001){
+        if(s->c[i] > EPSILON){
             return i;
         }
     }
     return -1;
 }
-int init(int m, int n, simplex_t* s){
-    
+
+void pretty_print(simplex_t* s);
+void pivot(simplex_t* s, int row, int col);
+int initial(simplex_t* s, int m, int n, double** a, double* b, double* c, double* x, double y, int* var);
+double xsimplex(int m, int n, double** a, double* b, double* c, double* x, double y, int* var, int h);
+int init(simplex_t* s, int m, int n, double** a, double* b, double* c, double* x, double y, int* var){
+    glob+=1;
     int k,r;
     s->m = m;
     s->n = n;
-    s->a = make_matrix(m,n);
-    s->b = make_vector(m);
-    s->c = make_vector(n);
-    s->x = make_vector(n+1);
-    
+    s->a = a;
+    s->b = b;
+    s->c = c;
+    s->x = x;
+    s->y = y;
+    s->var = var;
 
     for (int i = 0; i < n; i += 1){
         scanf("%lf", &s->c[i]);
@@ -62,11 +70,11 @@ int init(int m, int n, simplex_t* s){
         scanf("%lf", &s->b[i]);     
     }
 
-    if(s->var == NULL){
+    if (s->var == NULL){
         s->var = calloc(m+n+1, sizeof(int));
-        for(int i=0; i < m+n; i++){
-            s->var[i] = i;
-        }
+    for(int i=0; i < m+n; i++){
+        s->var[i] = i;
+    }
     }
     
     for (k = 0, r = 1 ; r < m; r++){
@@ -78,21 +86,209 @@ int init(int m, int n, simplex_t* s){
     return k;
 }
 
+void prepare(simplex_t* s, int k){
+    glob+=1;
+    int m = s->m;
+    int n = s->n;
+    int i;
+    //making room for x_m+n @ var[n] by moving var[n..n+m-1] one step
+    for(i = m +n; i > n; i-=1){
+        s->var[i]=s->var[i-1];
+    }
+    s->var[n] = m+n;
+    n = n+1;
+    for (i=0; i<m; i+=1){
+        s->a[i][n-1]=1;
+    }
+    free(s->x); /* NOTE: This might not be required*/
+    s->x = make_vector(m+n);
+    free(s->c); /* NOTE: This might not be required*/
+    s->c = make_vector(n);
+    s->c[n-1] = -1;
+    s->n = n;
+    pivot(s,k,n-1);
+}
+int initial(simplex_t* s, int m, int n, double** a, double* b, double* c, double* x, double y, int* var){
+    glob+=1;
+    int i,j,k;
+    double w;
+    k = init(s, m, n, a, b, c, x, y, var);
+    return 1;
+    /*
+    if (s->b[k]>=0){
+        return 1;
+    }
+    prepare(s,k);
+    n = s->n;
+    s->y = xsimplex(m,n,s->a, s->b, s->c, s->x,0, s->var,1);
+    for (i = 0; i < m+n; i+=1){
+        if (fabs(s->x[i])>EPSILON){
+            free(s->x);
+            free(s->c);
+            return 0;
+        } else {
+            break;
+        }
+    }
+    if (i>=n){
+        for (j = k = 0; k<n; k +=1){
+            if (fabs(s->a[i-n][k]) > fabs(s->a[i-n][j])){
+                j = k;
+            }
+        }
+        pivot(s, i-n, j);
+        i = j;
+    }
+    if (i < n-1){
+        k = s->var[i];
+        s->var[i] = s->var[n-1];
+        s->var[n-1] = k;
+        for(k = 0; k < m; k+=1){
+            w = s->a[k][n-1];
+            s->a[k][n-1] = s->a[k][i];
+            s->a[k][i] = w;
+        }
+    } else {
+        //we do nothing
+    }
+    free(s->c);
+    s->c = c;
+    //free(s->y) // TODO if we have memory leak
+    s->y = y;
+    for (k = n-1; k < n+m-1; k+=1){
+        s->var[k] = s->var[k+1];
+    }
+    n = s->n = s->n-1;
+    double* t = make_vector(n);
+    for (k = 0; k<n; k +=1){
+        for (j = 0; j < n; j+=1){
+            if (k == s->var[j]){
+                t[j] = t[j] + s->c[k];
+                goto next_k;
+            }
+        }
+        for (j = 0; j<m; j+=1){
+            if(s->var[n+j]==k){
+                break;
+            }
+        }
+        s->y = s->y + s->c[k]*s->b[j];
+        for(i = 0; i < n; i += 1){
+            t[i]=t[i]-s->c[k]*s->a[j][i];
+        }
+        next_k:;
+    }
+    for (i = 0; i < n; i+=1) s->c[i] = t[i];
+    free(t);
+    free(s->x);
+    return 1;
+    */
+}
+
+void pivot(simplex_t* s, int row, int col){
+    glob+=1;
+    double** a = s->a;
+    double* b = s->b;
+    double* c = s->c;
+    int m = s->m;
+    int n = s->n;
+    int i,j,t;
+    t = s->var[col];
+    s->var[col] = s->var[n+row];
+    s->var[n+row] = t;
+    s->y = s->y+c[col]*b[row]/a[row][col];
+    for (i = 0; i < n; i = i+1){
+        if (i != col){
+            c[i] = c[i]-c[col]*a[row][i]/a[row][col];
+        }
+    }
+    c[col] = - c[col]/a[row][col];
+    for(i=0; i<m;i=i+1){
+        if (i!=row){
+            for(j=0; j<n;j+=1){
+                if(j!=col){
+                    a[i][j] = a[i][j] - a[i][col]*a[row][j]/a[row][col];
+                }
+            }
+        }
+    }
+    for(i=0; i < m; i +=1){
+        if(i!=row){
+            a[i][col] = -a[i][col]/a[row][col];
+        }
+    }
+    b[row] = b[row]/a[row][col];
+    a[row][col]= 1/a[row][col];
+}
+
+double xsimplex(int m, int n, double** a, double* b, double* c, double* x, double y, int* var, int h){
+    glob+=1;
+    simplex_t* s;
+    s = malloc(sizeof(simplex_t));
+    int i, row, col;
+    if (!initial(s,m,n,a,b,c,x,y,var)){
+        free(s->var);
+        return NAN;
+    }
+    while ((col = select_nonbasic(s))>=0){
+        row = -1;
+        for (i = 0; i < m; i+=1){
+            if (a[i][col]>EPSILON && (row < 0 || b[i]/a[i][col] < b[row]/a[row][col])){
+                row = i;
+            }
+        }
+        if (row < 0){
+            free(s->var);
+            return INFINITY;
+        }
+        pivot(s, row,col);
+    }
+    if (h == 0){
+        for(i = 0; i <n; i+=1){
+            if (s->var[i]<n){
+                x[s->var[i]] = 0;
+            }
+        }
+        for(i = 0; i <m; i+=1){
+            if (s->var[n+i]<n){
+                x[s->var[n+i]] = s->b[i];
+            }
+        }
+        free(s->var);
+    } else {
+        for(i = 0; i < n; i+=1) x[i] = 0;
+        for(i = n; i < n+m; i+=1) x[i] = s->b[i-n];
+    }
+    pretty_print(s);
+    return s->y;
+}
+
+double simplex(int m, int n, double** a, double* b, double* c, double* x, double y){
+    glob+=1;
+    return xsimplex(m,n,a,b,c,x,y,NULL,0);
+}
+
 void pretty_print(simplex_t* s){
     printf("max z = %10.3lfx + %10.3lfy \n", s->c[0], s->c[1] );
     for(int i=0; i<2;i++){
     printf("%10.3lfx %10.3lfy \u2264 %10.3lf \n", s->a[i][0], s->a[i][1], s->b[i]);}
 }
 
-int main(int argc, char** argv){
-    int m,n,k;
-    simplex_t* s;
-    s = malloc(sizeof(simplex_t));
-    scanf("%d %d", &m, &n);
-    k = init(m,n,s);
-    pretty_print(s);
-    printf("%d", k);
 
+int main(int argc, char** argv){
+    int m,n;
+    //simplex_t* s;
+    //s = malloc(sizeof(simplex_t));
+    scanf("%d %d", &m, &n);
+    //pretty_print(s);
+    double** a = make_matrix(m,n+1);
+    double* b = make_vector(m);
+    double* x = make_vector(n+1);
+    double* c = make_vector(n);
+    double y;
+    double solution = simplex(m,n,a,b,c,x,y);
+    printf("\nz = %f\n", solution);
+    //TODO fix memleak
     return 0;
 }
 
